00adf124fc923429a91d34e52e4629f255fbbc6a
kakasoo <kscodebase@gmail.com>
2025-06-20 15:13:34 +0900

    test: add test case of compiler

---
+++ test/src/features/test/test_agent_test_correct_files.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import typia from "typia";
+
+import { TestGlobal } from "../../TestGlobal";
+import { prepare_agent_test } from "./internal/prepare_agent_test";
+
+const ROOT = `${__dirname}/../../..`;
+
+export const test_agent_test_correct_files = async () => {
+  if (TestGlobal.env.CHATGPT_API_KEY === undefined) return false;
+
+  const files = JSON.parse(
+    await fs.promises.readFile(
+      `${ROOT}/assets/error-histories/shopping-backend-test-response.json`,
+      "utf8",
+    ),
+  );
+
+  const state = await prepare_agent_test("shopping-backend");
+  const response = await state.agent.getContext().compiler.typescript({
+    files,
+  });
+
+  console.log(JSON.stringify(response, null, 2));
+  typia.assert<true>(response.type === "success");
+};
