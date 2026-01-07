3c9445c840eb268a90c3c23511cb415455eb1b32
Jeongho Nam <samchon.github@gmail.com>
2026-01-07 13:41:21 +0900

    fix testing model

--- test/interface.bash
+++ test/interface.bash
@@ -17,28 +17,28 @@ code results/qwen/qwen3-next-80b-a3b-instruct/reddit/interface
 code results/qwen/qwen3-next-80b-a3b-instruct/shopping/interface
 
 # individual testings
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project todo > test.complement.todo.log
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project bbs > test.complement.bbs.log
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project reddit > test.complement.reddit.log
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project shopping > test.complement.shopping.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project todo > test.prerequisite.todo.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project bbs > test.prerequisite.bbs.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project reddit > test.prerequisite.reddit.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-next-80b-a3b-instruct --project shopping > test.prerequisite.shopping.log
 
 ######################################################
-# qwen/qwen3-235b-a22b-2507
+# qwen/qwen3-30b-a3b-thinking-2507
 ######################################################
 # archiving
-pnpm run archive:go --vendor qwen/qwen3-235b-a22b-2507 --project todo --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.todo.log
-pnpm run archive:go --vendor qwen/qwen3-235b-a22b-2507 --project bbs --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.bbs.log
-pnpm run archive:go --vendor qwen/qwen3-235b-a22b-2507 --project reddit --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.reddit.log
-pnpm run archive:go --vendor qwen/qwen3-235b-a22b-2507 --project shopping --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.shopping.log
+pnpm run archive:go --vendor qwen/qwen3-30b-a3b-thinking-2507 --project todo --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.todo.log
+pnpm run archive:go --vendor qwen/qwen3-30b-a3b-thinking-2507 --project bbs --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.bbs.log
+pnpm run archive:go --vendor qwen/qwen3-30b-a3b-thinking-2507 --project reddit --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.reddit.log
+pnpm run archive:go --vendor qwen/qwen3-30b-a3b-thinking-2507 --project shopping --from interface --to interface > archive.qwen-qwen3-235b-a22b-2507.shopping.log
 
 # opening projects
-code results/qwen/qwen3-235b-a22b-2507/todo/interface
-code results/qwen/qwen3-235b-a22b-2507/bbs/interface
-code results/qwen/qwen3-235b-a22b-2507/reddit/interface
-code results/qwen/qwen3-235b-a22b-2507/shopping/interface
+code results/qwen/qwen3-30b-a3b-thinking-2507/todo/interface
+code results/qwen/qwen3-30b-a3b-thinking-2507/bbs/interface
+code results/qwen/qwen3-30b-a3b-thinking-2507/reddit/interface
+code results/qwen/qwen3-30b-a3b-thinking-2507/shopping/interface
 
 # individual testings
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-235b-a22b-2507 --project todo > test.complement.todo.log
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-235b-a22b-2507 --project bbs > test.complement.bbs.log
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-235b-a22b-2507 --project reddit > test.complement.reddit.log
-pnpm ts-node src/agent/interface.complement.ts --vendor qwen/qwen3-235b-a22b-2507 --project shopping > test.complement.shopping.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-30b-a3b-thinking-2507 --project todo > test.prerequisite.todo.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-30b-a3b-thinking-2507 --project bbs > test.prerequisite.bbs.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-30b-a3b-thinking-2507 --project reddit > test.prerequisite.reddit.log
+pnpm ts-node src/agent/interface.prerequisite.ts --vendor qwen/qwen3-30b-a3b-thinking-2507 --project shopping > test.prerequisite.shopping.log
